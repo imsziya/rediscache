5179f501c77491a8d09b3d14aac5d82798e117a1
MdShafique_Ziya <mdshafique.ziya@dellteam.com>
2024-05-29 12:54:45 +0530

    Initial Commit

---
+++ Src/RedisCache/Services/IRedisCacheService.cs
@@ -0,0 +1,13 @@
+﻿// Ignore Spelling: Redis
+
+using System.Threading.Tasks;
+
+namespace RedisCache.Services
+{
+    public interface IRedisCacheService
+    {
+        Task<bool> CreateAsync<T>(string key, T value, int TTL = default);
+        Task<T> GetAsync<T>(string key);
+        Task<bool> DeleteAsync(string key);
+    }
+}
